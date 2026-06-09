bb4de974511250c9941a29968d65bb8a76c44b0a
Gerb Stralko <gerb.stralko@gmail.com>
2026-06-09 10:04:30 -0700

    add low power mode and general ai code cleanup

---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define LOW_POWER_MODE true
+
+const unsigned long SENSOR_INTERVAL_NORMAL = 3000;
+const unsigned long SENSOR_INTERVAL_LOW_POWER = 60000;
+
+const unsigned long UPLOAD_INTERVAL_NORMAL = 30000;
+const unsigned long UPLOAD_INTERVAL_LOW_POWER = 300000; // 5 min
